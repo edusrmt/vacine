5f3b49ec17f59771de3de8179651f1fb37f0be3d
Eduardo Sarmento <cealvesarmento@gmail.com>
2020-12-20 23:23:54 -0300

    Cria frontend com Vue.js

---
+++ frontend/vacine/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <router-view />
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+* {
+  font-family: Montserrat, Helvetica, Arial, sans-serif;
+}
+
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  padding: 0 10vw 10vw 10vw;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+
+#app > div {
+  flex: 1;
+}
+
+button {
+  width: 100%;
+  min-height: 48px;
+  border-style: none;
+  border-radius: 5px;
+  font-size: 18px;
+}
+
+button.primary {
+  background-color: #356AEA;
+  color: #FFFFFF;
+}
+
+button.secondary {
+  background-color: #FFFFFF;
+  border: 1px solid #356AEA;
+  color: #356AEA;
+}
+</style>
---
+++ frontend/vacine/src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+<div class="home">
+  <img src="../assets/medicine.svg" alt="Ilustração com dois médicos" />
+  <img class="logo" src="../assets/logo.png" alt="Logotipo do Vacine" />
+  <div>
+    <button class="primary">Cadastrar-se</button>
+    <button class="secondary">Entrar</button>
+  </div>
+</div>
+</template>
+
+<style scoped>
+div.home {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: stretch;
+  padding-top: 3rem;
+}
+
+button.primary {
+  margin-bottom: 1rem;
+}
+
+img {
+  width: 100%;
+}
+
+img.logo {
+  width: 80%;
+  margin: 0 auto;
+}
+</style>
